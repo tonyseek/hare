66e3fef2bd787fd0270c98036ff442ba8ec8b033
Jiangge Zhang <tonyseek@gmail.com>
2018-03-08 13:34:27 +0800

    Fix the output path prefix in URLs

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name]-[hash:6].js',
-    publicPath: '/static',
+    publicPath: '/static/',
   },
   resolve: {
     modules: [
